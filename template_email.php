a8f3abc0a6d5af873385a4ecc2cfa3347e94dfe8
Richard Ambrosio <richard.ambrosio1@live.com>
2020-06-24 12:53:29 -0300

    Alterando layout do template de email

--- template_email.php
+++ template_email.php
@@ -1,25 +1,28 @@
-<h1>Tarefa: <?=$tarefa['nome']; ?></h1>
+<h1>Lembrete - Gerenciador de Tarefas</h1>
+<h2>Tarefa: <?=$tarefa['nome']; ?></h2>
 
-<p>
-    <strong>Concluída: </strong><?=traduz_concluida($tarefa['concluida']); ?>
-</p>
+<h3>Concluída</h3>
+<p><?=traduz_concluida($tarefa['concluida']); ?></p>
 
-<p>
-    <strong>Descrição: </strong><?=nl2br($tarefa['descricao']); ?>
-</p>
+<hr>
 
-<p>
-    <strong>Prazo: </strong><?=traduz_data_para_exibir($tarefa['prazo']); ?>
-</p>
+<h3>Descrição</h3>
+<p><?=nl2br($tarefa['descricao']); ?></p>
 
-<p>
-    <strong>Prioridade: </strong><?=traduz_prioridade($tarefa['prioridade']); ?>
-</p>
+<hr>
+
+<h3>Prazo</h3>
+<p><?=traduz_data_para_exibir($tarefa['prazo']); ?></p>
+
+<hr>
+
+<h3>Prioridade</h3>
+<p><?=traduz_prioridade($tarefa['prioridade']); ?></p>
+
+<hr>
 
 <?php if (count($anexos) > 0) : ?>
     <p><strong>Atenção!</strong> Esta tarefa contém anexos...</p>
 <?php endif; ?>
 
-<p>
-    Tenha um bom dia!
-</p>
+<p>Tenha um bom dia!</p>
