e6411252f416db161f52d80ffd6b43fb217d48af
Richard Ambrosio <richard.ambrosio1@live.com>
2020-06-30 20:17:31 -0300

    Alterando visualização dos emails enviados

--- views/template_email.php
+++ views/template_email.php
@@ -1,10 +1,32 @@
-<h1>Tarefa: <?php echo $tarefa->getNome(); ?></h1>
+<h1>Lembrete - Gerenciador de Tarefas</h1>
+<h2><?php echo $tarefa->getNome(); ?></h2>
 
-<p><strong>Concluída:</strong> <?php echo traduz_concluida($tarefa->getConcluida()); ?></p>
-<p><strong>Descrição:</strong> <?php echo nl2br($tarefa->getDescricao()); ?></p>
-<p><strong>Prazo:</strong> <?php echo traduz_data_para_exibir($tarefa->getPrazo()); ?></p>
-<p><strong>Prioridade:</strong> <?php echo traduz_prioridade($tarefa->getPrioridade()); ?></p>
+<hr>
+
+<h3>Concluída:</h3>
+<p><?php echo traduz_concluida($tarefa->getConcluida()); ?></p>
+
+<hr>
+
+<h3>Descrição:</h3>
+<p><?php echo nl2br($tarefa->getDescricao()); ?></p>
+
+<hr>
+
+<h3>Prazo:</h3>
+<p><?php echo traduz_data_para_exibir($tarefa->getPrazo()); ?></p>
+
+<hr>
+
+<h3>Prioridade:</h3>
+<p><?php echo traduz_prioridade($tarefa->getPrioridade()); ?></p>
+
+<hr>
 
 <?php if (count($tarefa->getAnexos()) > 0) : ?>
     <p><strong>Atenção!</strong> Esta tarefa contém anexos!</p>
 <?php endif; ?>
+
+<hr>
+
+<h4>Tenha em excelente dia! ;)</h4>
