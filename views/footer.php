74c81aea6e14a85a0b3695f3ddbdb52622327d5f
Richard Ambrosio <richard.ambrosio1@live.com>
2020-06-30 21:29:17 -0300

    Separando o rodapé para poder reaproveitá-lo

---
+++ views/footer.php
@@ -0,0 +1,24 @@
+<footer class="text-light text-center bg-secondary">
+    <div class="container-fluid">
+        Desenvolvido por: <strong>Richard Ambrosio<strong>
+        
+        <div class="row">
+            <div class="col">
+                <a href="https://www.instagram.com/richa.ambrosio/" class="text-light">
+                    <i class="fa fa-instagram fa-2x" aria-hidden="true"></i>
+                </a>
+                &nbsp;&nbsp;
+                <a href="https://github.com/richardambrosio" class="text-light">
+                    <i class="fa fa-github fa-2x" aria-hidden="true"></i>
+                </a>
+                &nbsp;&nbsp;
+                <a href="https://www.facebook.com/richard.ambrosio97/" class="text-light">
+                    <i class="fa fa-facebook fa-2x" aria-hidden="true"></i>
+                </a>
+            </div>
+        </div>
+
+        <script src="bibliotecas/bootstrap-4.4.1-dist/css/jquery-3.5.1.min.js"></script>
+        <script src="bibliotecas/bootstrap-4.4.1-dist/css/bootstrap.bundle.min.js"></script>
+    </div>
+</footer>
