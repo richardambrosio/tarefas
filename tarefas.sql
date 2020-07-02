72fc43cca666638df07dfc4f0e1806b123625b14
Richard Ambrosio <richard.ambrosio1@live.com>
2020-07-02 15:51:01 -0300

    Script de criação da base de dados

--- tarefas.sql
+++ tarefas.sql
@@ -1,86 +1,36 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 01-Jul-2020 às 02:39
--- Versão do servidor: 10.4.11-MariaDB
--- versão do PHP: 7.2.29
+CREATE DATABASE tarefas;
+USE tarefas;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE TABLE anexos (
+  id int(11) NOT NULL,
+  tarefa_id int(11) NOT NULL,
+  nome varchar(255) NOT NULL,
+  arquivo varchar(255) NOT NULL
+);
 
+CREATE TABLE tarefas (
+  id int(11) NOT NULL,
+  nome varchar(20) NOT NULL,
+  descricao text DEFAULT NULL,
+  prazo date DEFAULT NULL,
+  prioridade int(1) DEFAULT NULL,
+  concluida tinyint(1) DEFAULT NULL
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+ALTER TABLE anexos
+  ADD PRIMARY KEY (id),
+  ADD KEY fk_idTarefa_anexos (tarefa_id);
 
---
--- Banco de dados: `tarefas`
---
+ALTER TABLE tarefas
+  ADD PRIMARY KEY (id);
 
--- --------------------------------------------------------
+ALTER TABLE anexos
+  MODIFY id int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
---
--- Estrutura da tabela `anexos`
---
+ALTER TABLE tarefas
+  MODIFY id int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 
-CREATE TABLE `anexos` (
-  `id` int(11) NOT NULL,
-  `tarefa_id` int(11) NOT NULL,
-  `nome` varchar(255) NOT NULL,
-  `arquivo` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+ALTER TABLE anexos
+  ADD CONSTRAINT fk_idTarefa_anexos FOREIGN KEY (tarefa_id) REFERENCES tarefas (id);
 
---
--- Estrutura da tabela `tarefas`
---
-
-CREATE TABLE `tarefas` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(20) NOT NULL,
-  `descricao` text DEFAULT NULL,
-  `prazo` date DEFAULT NULL,
-  `prioridade` int(1) DEFAULT NULL,
-  `concluida` tinyint(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Índices para tabela `anexos`
---
-ALTER TABLE `anexos`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_idTarefa_anexos` (`tarefa_id`);
-
---
--- Índices para tabela `tarefas`
---
-ALTER TABLE `tarefas`
-  ADD PRIMARY KEY (`id`);
-
-
---
--- AUTO_INCREMENT de tabela `anexos`
---
-ALTER TABLE `anexos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
---
--- AUTO_INCREMENT de tabela `tarefas`
---
-ALTER TABLE `tarefas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
-
-
---
--- Limitadores para a tabela `anexos`
---
-ALTER TABLE `anexos`
-  ADD CONSTRAINT `fk_idTarefa_anexos` FOREIGN KEY (`tarefa_id`) REFERENCES `tarefas` (`id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+GRANT ALL PRIVILEGES ON tarefas.* TO 'sistema_tarefas'@'localhost' IDENTIFIED BY 'sistema';
