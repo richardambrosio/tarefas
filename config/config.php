1e63cff932a3cb759aa4baf28127dc7238a3eda6
Richard Ambrosio <richard.ambrosio1@live.com>
2020-07-02 15:59:55 -0300

    Constantes usadas na base de dados/envio de email

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+# Conexão com o banco de dados (MySql)
+define('BD_USUARIO', 'sistema_tarefas');
+define('BD_SENHA', 'sistema');
+define('BD_DSN', 'mysql:dbname=tarefas;host=localhost');
+
+# E-mail para notificação
+define('EMAIL_NOTIFICACAO', 'Email que receberá a notificação'); #Usar o seu próprio, para testes
+define('EMAIL_REMETENTE', 'Seu email');
+define('SENHA_REMETENTE', 'Sua senha');
